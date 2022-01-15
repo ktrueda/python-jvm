dc7a3999d7acdb7c02ca27b14e060b11939c3eb7
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-01-15 14:49:19 +0900

    WIP

--- tests/test_class_parser.py
+++ tests/test_class_parser.py
@@ -4,48 +4,53 @@ from python_jvm.class_parser import read_classfile
 from python_jvm.executer import execute, find_code, find_method, load_classes
 import subprocess
 
+
 @pytest.fixture(params=[
     'HelloWorld.class',
     # 'Print.class'
-    ])
+])
 def classfile_path(request):
     return request.param
 
+
 def test_read_classfile(classfile_path):
     '''Test it can parse all class file without Exception'''
-    read_classfile(classfile_path) 
+    read_classfile(classfile_path)
+
 
 def test_load_classes():
     actual = load_classes('./*.class')
-    assert actual.keys() == {"HelloWorld", "Print"}
+    assert actual.keys() == {"HelloWorld", "Print", "java/lang/Object", "Person"}
+
 
 stdout = ""
+
+
 def test_execute_classfile(classfile_path, mocker):
     global stdout
     cfs = load_classes('./*.class')
-    cf = read_classfile(classfile_path) 
+    cf = read_classfile(classfile_path)
     main_method = find_method(cfs, 'HelloWorld', 'main')
 
     stdout = ""
+
     def append_stdout(text):
         global stdout
         stdout += str(text) + "\n"
     if main_method:
         mocker.patch("python_jvm.executer.std_method", {
             'java/lang/System': {
-                'out':{
-                    'println': lambda x:append_stdout(x[0]) 
+                'out': {
+                    'println': lambda x: append_stdout(x[0])
                 }
             }
         })
         target_class = ''.join(classfile_path.split('.')[:-1])
         expected = subprocess.check_output(
             f'java -cp . {target_class}',
-            shell=True, 
+            shell=True,
             # stderr=subprocess.STDOUT,
             cwd='.').decode()
         main_method_code = find_code(main_method, cfs, 'HelloWorld')
-        execute(main_method_code, cfs, 'HelloWorld', [None for _ in range(main_method_code.max_locals)])
+        execute(main_method_code, cfs, 'HelloWorld', [None for _ in range(main_method_code.max_locals)], {})
         assert stdout == expected
-    
-
