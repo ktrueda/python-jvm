294c076ef64a7faf52f8bd2481be56e97b823daf
Kentaro Ueda <kentaro.ueda.kentaro@gmail.com>
2022-03-12 15:03:28 +0900

    Pass test

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,76 +2,71 @@ import java.util.function.Supplier;
 
 class HelloWorld{
   public static void main(String[] args){
-    // System.out.println("hello");
-    // int i = 1;
-    // int j = i + 2;
-    // System.out.println(j);
-    // System.out.println(3+4);
+    System.out.println("hello");
+    int i = 1;
+    int j = i + 2;
+    System.out.println(j);
+    System.out.println(3+4);
 
-    // for(int l = 0; l < 2; l++){
-    //   System.out.println("progress");
-    // }
-    // int s = 0;
-    // for(int l = 1; l < 5; l++){
-    //   s += l;
-    // }
-    // System.out.println(s);
-    // show();
-    // int c = constant();
-    // System.out.println(c);
+    for(int l = 0; l < 2; l++){
+      System.out.println("progress");
+    }
+    int s = 0;
+    for(int l = 1; l < 5; l++){
+      s += l;
+    }
+    System.out.println(s);
+    show();
+    int c = constant();
+    System.out.println(c);
 
-    // int r = identity(12345);
-    // System.out.println(r);
+    int r = identity(12345);
+    System.out.println(r);
 
-    // System.out.println(add(1,30));
+    System.out.println(add(1,30));
 
-    // System.out.println(fibonacci(15));
+    System.out.println(fibonacci(15));
 
-    // Print.print(98765);
+    Print.print(98765);
 
-    // newInstance();
+    newInstance();
 
-    lambda();
+    // lambda();
   }
 
-  // public static void show(){
-  //   System.out.println("show string");
-  // }
-
-  // public static int constant(){
-  //   return 1234;
-  // }
-
-  // public static int identity(int arg){
-  //   return arg;
-  // }
-
-  // public static int add(int x, int y){
-  //   return x + y;
-  // }
-
-  // public static int fibonacci(int n){
-  //   if(n == 0){
-  //     return 1;
-  //   }else if(n == 1){
-  //     return 1;
-  //   }else{
-  //     return fibonacci(n-1) + fibonacci(n-2);
-  //   }
-  // }
-
-  // public static void newInstance(){
-  //   Person p = new Person("Chisato", 17);
-  //   System.out.println(p.getName());
-  // }
-
-  public static void lambda(){
-
-    Supplier<String> lambda = () ->{ 
-      System.out.println("Lambda in progress");
-      return "Hello from supplier";
-    };
-    String text = lambda.get();
-    System.out.println(text);
+  public static void show(){
+    System.out.println("show string");
   }
+
+  public static int constant(){
+    return 1234;
+  }
+
+  public static int identity(int arg){
+    return arg;
+  }
+
+  public static int add(int x, int y){
+    return x + y;
+  }
+
+  public static int fibonacci(int n){
+    if(n == 0){
+      return 1;
+    }else if(n == 1){
+      return 1;
+    }else{
+      return fibonacci(n-1) + fibonacci(n-2);
+    }
+  }
+
+  public static void newInstance(){
+    Person p = new Person("Chisato", 17);
+    System.out.println(p.getName());
+  }
+
+  // public static void lambda(){
+  //   Runnable r = () -> System.out.println("Hell");
+  //   r.run();
+  // }
 }
